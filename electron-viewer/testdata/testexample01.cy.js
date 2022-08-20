c1a46294cccdc6da209acd21668769d7918102ca
AMATERASU\BBE <services@benedict.es>
2022-08-20 20:59:13 +0200

    feat: added Testdata

---
+++ electron-viewer/testdata/testexample01.cy.js
@@ -0,0 +1,19 @@
+define('title',()=>{
+    it('subtitle1',()=>{
+        //ARRANGE GotoPage
+        //ACT Press Button
+        //ASSERT All Good
+    })
+    it('subtitle2',()=>{
+        //ARRANGE GoToPage
+        //ARRANGE Login
+        //ACT Press Button
+        //ASSERT All Bad
+    })
+    xit('subtitle3',()=>{
+        //ARRANGE GoToPage
+        //ARRANGE Login
+        //ACT Press Button
+        //ASSERT All Bad
+    })
+})
